f1ce551b97cff78e81d278baf07646cec497e462
Florian Benkö <florian.benkoe@uni-konstanz.de>
2020-10-29 18:06:30 +0100

    setup py intial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='waipawama',
+      version='0.1',
+      description='Data Pipeline Project.',
+      url='github.com/elcolumbio/waipawama',
+      author='Florian Benkö',
+      author_email='f.benkoe@innotrade24.de',
+      license='MIT',
+      packages=find_packages())
