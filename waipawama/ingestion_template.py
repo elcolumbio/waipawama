170732d60728e1b8fa759b33f9df9e20aaefc1c8
elcolumbio <20401370+elcolumbio@users.noreply.github.com>
2020-12-31 19:46:01 +0100

    bigquery ingestion function

---
+++ waipawama/ingestion_template.py
@@ -0,0 +1,113 @@
+from pydantic import (
+    BaseModel as PydanticBaseModel,
+    Field,
+    FilePath,
+    validator,
+    ValidationError)
+import pyarrow as pa
+import pyarrow.parquet as pq
+import datetime
+import numpy as np
+import pandas as pd
+import pathlib
+from typing import List, Optional
+from math import isnan
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+
+
+class BaseModel(PydanticBaseModel):
+    """Cast np.nan from pandas to None and potential ValidationError."""
+    @validator('*', pre=True)
+    def change_nan_to_none(cls, v, field):
+        if type(v) is float and isnan(v):
+            return None
+        return v
+
+
+class Meta(PydanticBaseModel):
+    """Wrapper with basic function to prepare and push data to bigquery."""
+
+    def rename_columns(self):
+        # dynamic parsing from our model, we could do more things like that.
+        rename_columns = {
+            model.alias: field for field, model in self.model.__fields__.items()}
+        
+        df = self.read_csv()
+        # ignore missing columns because alias can deprecate
+        df.rename(rename_columns, axis=1, inplace=True, errors='ignore')
+        return df
+    
+    def save_as_parquet(self):
+        dfrenamed = self.rename_columns()
+        dfrenamed['Timespan'] = self.timespan
+        dfrenamed['TimeInsert'] = pd.Timestamp.now(tz='utc')
+        dfrenamed.to_parquet(self.tmp_file, index=False)
+
+    def read_parquet(self):
+        return pd.read_parquet(self.tmp_file)
+
+    @property
+    def TableExists(self):
+        client = bigquery.Client()
+        try:
+            client.get_table(self.table_id)  # Make an API request.
+            return True
+        except NotFound:
+            return False
+        
+    def create_schema(self, pydantic_schema):
+        """The pydantic schema you get like this: self.model.schema(by_alias=False) ."""
+        required = pydantic_schema['required']
+        schema = []
+        for column, model in pydantic_schema['properties'].items():
+            if column in required:
+                mode = 'REQUIRED'
+            else:
+                mode = 'NULLABLE'
+            dtype = model['type'].upper()
+            if dtype == 'NUMBER':
+                dtype = 'FLOAT'
+            elif model.get('format') == 'date' or model.get('format') == 'date-time':
+                dtype = 'TIMESTAMP'
+        
+                
+            description = model.get('description', None)
+            schema.append(bigquery.SchemaField(
+                column, dtype, mode=mode, description=description))
+        return schema
+    
+    def create_table(self):
+        """Create schema from pydantic model and push."""
+        client = bigquery.Client()
+        schema = self.create_schema(self.model.schema(by_alias=False))
+        table = bigquery.Table(self.table_id, schema=schema)
+        table = client.create_table(table)
+        return table
+    
+    def update_table(self):
+        """Create schema from pydantic model, bigquery only allows some changes."""
+        client = bigquery.Client()
+        schema = self.create_schema(self.model.schema(by_alias=False))
+        table = bigquery.Table(self.table_id, schema=schema)
+        table = client.update_table(table, ['schema'])
+        return table
+        
+    def append_data(self):
+        """Append data and update schema."""
+        client = bigquery.Client()
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+        # most powerful schema update
+        job_config.schema_update_options = [
+            bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+            bigquery.SchemaUpdateOption.ALLOW_FIELD_RELAXATION]
+        job_config.schema = self.create_schema(self.model.schema(by_alias=False))
+        job_config.source_format = bigquery.SourceFormat.PARQUET
+        
+        with open(self.tmp_file, "rb") as source_file:
+            job = client.load_table_from_file(
+                source_file,
+                self.table_id,
+                job_config=job_config,)
+        return job.result()
---
+++ waipawama/jtl_invoice.py
@@ -0,0 +1,79 @@
+from pydantic import (
+    BaseModel as PydanticBaseModel,
+    Field,
+    FilePath,
+    validator,
+    ValidationError)
+import pyarrow as pa
+import pyarrow.parquet as pq
+import datetime
+import numpy as np
+import pandas as pd
+import pathlib
+from typing import List, Optional
+from math import isnan
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+
+from .ingestion_template import (
+    BaseModel,
+    Meta)
+
+
+class JtlInvoice(BaseModel):
+    """Input model after parsing used for testing, aliasing and dtypes."""
+
+    Rechnungsnummer: str = Field(description='Auto generated invoice number by our inventory management system.')
+    BetragBrutto: float = Field(alias = 'Gesamtbetrag Brutto (alle Ust.)', description='For all positions in the invoice together.')
+    DatumRechnung: datetime.datetime = Field(alias='Erstelldatum Rechnung', description='Relevant date for period of taxation.')
+    Steuerschluessel: Optional[int] = Field(alias='Steuerschlüssel', description='For each VAT case there is a number.')
+    # resolve in transformation just addd new fields here
+    Steuerschluessel2: Optional[int] = Field(alias='Steuerschlüsselnummer', description='Another entrypoint for Steuerschluessel.') 
+    ExterneId: Optional[str] = Field(alias='Externe Transaktion-ID', description='For example the transaction id from another platform like ebay.')
+        
+    # Meta Columns
+    Timespan: str = Field(description='The key parameter for every run of the complete datapipeline, represents a VAT period.')
+    TimeInsert: datetime.datetime = Field(description='Timestamp when we insert the data, time is UTC and not local Time.')
+
+
+class JtlMeta(Meta):
+    """Wrapper for all the python functions for JtlData Model."""    
+    
+    timespan: str = ''
+
+    data: Optional[List[JtlInvoice]] = None # for  second step validating
+    table_id: str = 'waipa-1.test.JtlInvoice'
+    model = JtlInvoice  # We don't instantiate the Model with data
+
+
+    # somehow duplicate but it's explicit and easy to read
+    dtypes: dict = {
+            'Rechnungsnummer': object,
+            'Gesamtbetrag Brutto (alle Ust.)': np.float64,
+            'Erstelldatum Rechnung': object,
+            'Steuerschlüssel': int,
+            'Steuerschlüsselnummer': int,
+            'Externe Transaktion-ID': object}
+    parse_dates=['Erstelldatum Rechnung']
+
+    @property
+    def data_file(self):
+        """Set first part in env."""
+        return pathlib.Path('/home/flo/Nextcloud/data/finance/', self.timespan, 'jtl_ebay.csv')
+
+    @property
+    def tmp_file(self):
+        base_folder = '/home/flo/tests/'
+        pathlib.Path(base_folder, self.timespan).mkdir(parents=True, exist_ok=True)
+        return pathlib.Path('/home/flo/tests/', self.timespan, 'jtl_ebay.csv')
+
+    def read_csv(self):
+        return pd.read_csv(
+            self.data_file,
+            dtype=self.dtypes,
+            encoding='latin-1',
+            sep=';',
+            decimal=',',
+            thousands=None,
+            dayfirst=True,
+            parse_dates=self.parse_dates)
